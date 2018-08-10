18b4f3142355441d9d6ecb87d54471c0e1640461
Stan Janssen <mail@janssen.io>
2018-08-10 11:47:06 +0200

    Remove old test project

--- tests/Temporal.Core.Domain.Tests/Tests.fs
+++ tests/Temporal.Core.Domain.Tests/Tests.fs
@@ -1,10 +1,16 @@
 module Tests
 
+open System
 open FsCheck.Xunit
 open Temporal.Core.Domain.Computation
 
 type Dep = Map<string, int>
 let sumMap = Map.fold (fun acc _ value -> acc + value) 0
+
+let filterEmpty = List.filter (not << String.IsNullOrWhiteSpace)
+let depCount n = n * (n-1) / 2
+let maxMap: Map<string * string, int> -> int =
+    Map.fold (fun acc _ count -> if count > acc then count else acc) 0
 
 [<Property>]
 let ``Adding a dependency increments the total count`` dep (deps:Dep) =
@@ -15,3 +21,15 @@ let ``Adding a dependency increments the total count`` dep (deps:Dep) =
 let ``The sum of a merged mapping is equal to the individual sums`` (groupA:Dep) (groupB:Dep) =
     let groupAB = mergeDependencies groupA groupB
     sumMap groupAB = (sumMap groupA + sumMap groupB)
+
+[<Property>]
+let ``Makes pairs of all different file names`` xs =
+    let xs' = (List.distinct >> filterEmpty) xs
+    let n = List.length xs'
+    computeTemporalDependencies [xs']
+    |> Map.count = depCount n
+
+[<Property>]
+let ``Maximum count is at most equal to number of commits`` commits =
+    computeTemporalDependencies commits
+    |> maxMap <= List.length commits 
